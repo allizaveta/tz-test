06aff77d36ea35485ba7ae9306a9be6cb27bb278
Allizaveta <allizaveta@gmail.com>
2024-10-08 16:15:22 +0300

    3 теста

--- src/__test__/UserList.test.tsx
+++ src/__test__/UserList.test.tsx
@@ -1,6 +1,19 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import UserList from "../components/userList/UserList";
+
+interface UserDetailsProps {
+  user: any;
+  onClose: () => void;
+}
+
+jest.mock("../components/userDetails/userDetails", () => {
+  return jest.fn(({ onClose }: UserDetailsProps) => (
+    <div>
+      <button onClick={onClose}>Close</button>
+    </div>
+  ));
+});
 
 describe("UserList", () => {
   const users = [
@@ -13,5 +26,26 @@ describe("UserList", () => {
     users.forEach((user) => {
       expect(getByText(user.login)).toBeInTheDocument();
     });
+  });
+
+  it("opens UserDetails popup when a user is clicked", () => {
+    const { getByText } = render(<UserList users={users} />);
+    fireEvent.click(getByText("user1"));
+    expect(
+      require("../components/userDetails/userDetails")
+    ).toHaveBeenCalledWith(
+      { user: users[0], onClose: expect.any(Function) },
+      {}
+    );
+  });
+
+  it("closes UserDetails popup when the close function is called", () => {
+    const UserDetails = require("../components/userDetails/userDetails");
+
+    const { getByText, queryByText } = render(<UserList users={users} />);
+    fireEvent.click(getByText("user1"));
+    expect(UserDetails).toHaveBeenCalled();
+    fireEvent.click(getByText("Close"));
+    expect(queryByText("Close")).not.toBeInTheDocument();
   });
 });
